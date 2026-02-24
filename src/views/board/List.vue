814ef7a103092cc2556c10f1ade234422e376d07
Tahcy-99 <sjae999@gmail.com>
2026-02-24 14:01:53 +0900

    [Feat] 페이지 선택 추가

--- src/views/board/List.vue
+++ src/views/board/List.vue
@@ -7,7 +7,30 @@ const page = route.query.page
 </script>
 
 <template>
-  <BoardList :page="page"></BoardList>
+  <div v-if="page">
+    <BoardList :page="page"></BoardList>
+  </div>
+  <div v-else>
+    <div>페이지 번호 선택</div>
+  </div>
+  <div class="pageList">
+    <a class="pageNumber" :href="`/board/list?page=1`">1</a>
+    <a class="pageNumber" :href="`/board/list?page=2`">2</a>
+    <a class="pageNumber" :href="`/board/list?page=3`">3</a>
+    <a class="pageNumber" :href="`/board/list?page=4`">4</a>
+    <a class="pageNumber" :href="`/board/list?page=5`">5</a>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+a {
+  text-decoration: none;
+  color: inherit;
+}
+.pageList {
+  display: flex;
+}
+.pageNumber {
+  flex: 1;
+}
+</style>
